62db43b1cf15758fd632fe44abeeb96ab5f871ab
islno <islaniopbl@gmail.com>
2024-12-16 15:15:26 -0300

    Clarified variable and object comparison in Java

---
+++ src/TestesMalucos/Igual.java
@@ -0,0 +1,28 @@
+package TestesMalucos;
+
+public class Igual {
+    String nome;
+
+    public static void main(String[] args) {
+        Igual a1 = new Igual();
+        a1.nome = "a";
+
+        Igual b1 = new Igual();
+        b1.nome = "a";
+
+        if (a1 == b1) {
+            System.out.println("Iguais");
+        } else {
+            System.out.println("Diferentes");
+        }
+
+        String a = "a";
+        String b = "a";
+
+        if (a == b) {
+            System.out.println("Iguais");
+        } else {
+            System.out.println("Diferentes");
+        }
+    }
+}
